6f71bb43aec7b4b89584d8fc454c2a57adf272bd
Dmitrii Balakhonskii <dnomaid@gmail.com>
2019-01-23 21:15:30 +0100

    use Clojure 1.10 for dev

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Library for keeping up-to-date text file contents in memory."
   :url "http://github.com/dryewo/mem-files"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies []
   :plugins [[lein-cloverage "1.0.13"]
             [lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]
             [lein-changelog "0.3.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
   :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[me\\\\.dryewo\\\\/mem-files \"[0-9.]*\"\\\\]/[me\\\\.dryewo\\\\/mem-files \"${:version}\"]/" "README.md"]}
   :release-tasks [["shell" "git" "diff" "--exit-code"]
